f6f48aabba5cbaffb885418d249beddb3e28dc18
Sakar Michel <sakar.michel@gmail.com>
2021-03-04 22:12:06 -0500

    feat: Add Pending Intent
    When the users taps on the notification in the Notify Me App will open up.

--- NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.core.app.NotificationCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -55,11 +57,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private NotificationCompat.Builder getNotificationBuilder(){
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
 
         NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID);
         notifyBuilder.setContentTitle("You've been notified!");
         notifyBuilder.setContentText("This is your notification text.");
         notifyBuilder.setSmallIcon(R.drawable.ic_android);
+
+        notifyBuilder.setContentIntent(notificationPendingIntent).setAutoCancel(true);
+
 
         return notifyBuilder;
     }
